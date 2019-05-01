8fb00b502c02bf6dd3b6a1d5043b9b4bf6019b1b
Kieran Trezona-le Comte <trezona.lecomte@gmail.com>
2019-05-01 21:14:28 +1200

    Add rating to Movie model

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -16,7 +16,8 @@ namespace RazorPagesMovie.Models
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
-        [Column(TypeName = "decimal(18, 2")]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
